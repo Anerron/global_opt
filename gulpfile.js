259127089a0998ad1f85114eef8ef06d5e8ca7e8
Arhtur <arturkendys87@gmail.com>
2026-02-03 23:38:43 +0300

    Files are ready to work

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create(); // Обратите внимание на .create()
+const sass = require('gulp-sass')(require('sass'));
+const rename = require("gulp-rename");
+const autoprefixer = require("gulp-autoprefixer").default || require("gulp-autoprefixer");
+const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+
+// Static server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        }
+    });
+
+    gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/scss/**/*.+(sass|scss)")
+            .pipe(sass().on('error', sass.logError))
+            .pipe(rename({
+                prefix: "",
+                suffix: ".min",
+            }))
+            .pipe(autoprefixer({
+			    cascade: false
+		    }))
+            .pipe(cleanCSS({compatibility: 'ie8'}))
+            .pipe(gulp.dest("dist/css"))
+            .pipe(browserSync.stream());
+});
+
+gulp.task("watch", function() {
+    gulp.watch("src/sсss/**/*.+(sass|scss|css)").on("change", gulp.parallel("styles"));
+    gulp.watch("*.html").on("change", gulp.parallel('html'));
+    gulp.watch("src/js/**/*.js").on("change", gulp.parallel('scripts'));
+    gulp.watch("src/img/**/*").on("change", gulp.parallel('images'));
+
+});
+
+gulp.task('html', function() {
+    return gulp.src("*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src("src/js/**/*")
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('images', function() {
+    // динамически импортируем ESM-модуль внутри таска
+    return import('gulp-imagemin').then(({ default: imagemin }) => {
+        return gulp.src("src/imgs/**/*", { encoding: false })
+            .pipe(imagemin())
+            .pipe(gulp.dest('dist/imgs'))
+            .pipe(browserSync.stream());
+    });
+});
+
+gulp.task("default", gulp.parallel("watch", "server", "styles", "scripts",  "html", "images"));

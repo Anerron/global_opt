af5664cf5c298b836c9800cf51aded8a07a9f372
Arhtur <arturkendys87@gmail.com>
2026-02-16 23:56:00 +0300

    Added adaptive section

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,7 +3,6 @@ const overlay = document.querySelector('.header__overlay')
         menu = document.querySelector('.header__menu_mobile')
         menuClose = document.querySelector('.header__menu__close');
 
-
 hamburger.addEventListener('click', () => {
     overlay.classList.add('active');
     menu.classList.add('active');
@@ -13,3 +12,26 @@ menuClose.addEventListener('click', () => {
     overlay.classList.remove('active');
     menu.classList.remove('active');
 });
+
+function validateForms(form){
+    $(form).validate({
+        rules: {
+        name: "required",
+        phone: "required",
+        email: {
+            required: true,
+            email: true
+        }
+        },
+        messages: {
+        name: "Пожалуйста, введите своё имя",
+        phone: "Пожалуйста, введите номер телефона",
+        email: {
+            required: "Пожалуйста, введите почту",
+            email: "Неправильно введён адрес почты"
+        }
+        }
+    });
+}
+
+validateForms('#variants-form');
